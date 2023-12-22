cd678b42e0f9ff8b9a13b7a53fcc100d895c8a30
Ritankar Saha <ritankar.saha786@gmail.com>
2023-12-22 12:51:30 +0530

    JAVASCRIPT FILE

--- calculator.js
+++ calculator.js
@@ -1,47 +1,36 @@
-var screen=document.querySelector('#screen');
-var btn=document.querySelectorAll('.button');
+var screen = document.querySelector("#screen");
+var btn = document.querySelectorAll(".button");
 
+for (item of btn) {
+  item.addEventListener("click", (e) => {
+    btntext = e.target.innerText;
 
-for(item of btn)
-{
-    item.addEventListener('click',(e)=>{
-        btntext=e.target.innerText;
+    if (btntext == "x") {
+      btntext = "*";
+    }
 
-        if(btntext =='x')
-        {
-            btntext= '*';
-        }
-
-        if(btntext=='÷')
-        {
-            btntext='/';
-        }
-        screen.value+=btntext;
-    });
+    if (btntext == "÷") {
+      btntext = "/";
+    }
+    screen.value += btntext;
+  });
 }
-function check()
-{
-  if (screen.value < 0 )
-  {
+function check() {
+  if (screen.value < 0) {
     screen.value = Math.abs(screen.value);
-  }
-  else if(screen.value > 0)
-  {
+  } else if (screen.value > 0) {
     screen.value = 0 - screen.value;
-  }
-  else
-  {
+  } else {
     screen.value = screen.value;
   }
 }
-function sin()
-{
-    screen.value=Math.sin(screen.value);
+function sin() {
+  screen.value = Math.sin(screen.value);
 }
 
 function sinInverse() {
   var inputValue = parseFloat(screen.value);
-  
+
   if (isNaN(inputValue)) {
     alert("Invalid input. Please enter a valid number.");
     return;
@@ -56,13 +45,12 @@ function sinInverse() {
   screen.value = result;
 }
 
-function cosec()
-{
-    screen.value=1/(Math.sin(screen.value));
+function cosec() {
+  screen.value = 1 / Math.sin(screen.value);
 }
 function cosecInverse() {
   var inputValue = parseFloat(screen.value);
-  
+
   if (isNaN(inputValue) || inputValue === 0) {
     alert("Invalid input. Please enter a non-zero valid number.");
     return;
@@ -71,13 +59,12 @@ function cosecInverse() {
   var result = Math.asin(1 / inputValue) * (180 / Math.PI);
   screen.value = result;
 }
-function cos()
-{
-    screen.value=Math.cos(screen.value);
+function cos() {
+  screen.value = Math.cos(screen.value);
 }
 function cosInverse() {
   var inputValue = parseFloat(screen.value);
-  
+
   if (isNaN(inputValue)) {
     alert("Invalid input. Please enter a valid number.");
     return;
@@ -91,13 +78,12 @@ function cosInverse() {
   var result = Math.acos(inputValue) * (180 / Math.PI);
   screen.value = result;
 }
-function sec()
-{
-    screen.value=1/(Math.cos(screen.value));
+function sec() {
+  screen.value = 1 / Math.cos(screen.value);
 }
 function secInverse() {
   var inputValue = parseFloat(screen.value);
-  
+
   if (isNaN(inputValue) || inputValue === 0) {
     alert("Invalid input. Please enter a non-zero valid number.");
     return;
@@ -106,13 +92,12 @@ function secInverse() {
   var result = Math.acos(1 / inputValue) * (180 / Math.PI);
   screen.value = result;
 }
-function tan()
-{
-    screen.value=Math.tan(screen.value);
+function tan() {
+  screen.value = Math.tan(screen.value);
 }
 function tanInverse() {
   var inputValue = parseFloat(screen.value);
-  
+
   if (isNaN(inputValue)) {
     alert("Invalid input. Please enter a valid number.");
     return;
@@ -121,13 +106,12 @@ function tanInverse() {
   var result = Math.atan(inputValue) * (180 / Math.PI);
   screen.value = result;
 }
-function cot()
-{
-    screen.value=1/(Math.tan(screen.value));
+function cot() {
+  screen.value = 1 / Math.tan(screen.value);
 }
 function cotInverse() {
   var inputValue = parseFloat(screen.value);
-  
+
   if (isNaN(inputValue) || inputValue === 0) {
     alert("Invalid input. Please enter a non-zero valid number.");
     return;
@@ -136,102 +120,93 @@ function cotInverse() {
   var result = Math.atan(1 / inputValue) * (180 / Math.PI);
   screen.value = result;
 }
-function reverse()
-{
-    screen.value=1/screen.value;
+function reverse() {
+  screen.value = 1 / screen.value;
 }
-function pow()
-{
-    screen.value=Math.pow(screen.value,2);
+function pow() {
+  screen.value = Math.pow(screen.value, 2);
 }
 
-function sqrt()
-{
-    screen.value=Math.sqrt(screen.value);
+function sqrt() {
+  screen.value = Math.sqrt(screen.value);
 }
-function mod()
-{
-  screen.value=Math.abs(screen.value);
+function mod() {
+  screen.value = Math.abs(screen.value);
 }
-function log()
-{
-    screen.value=Math.log(screen.value);
+function log() {
+  screen.value = Math.log(screen.value);
 }
 
-function pi()
-{
-    screen.value= 3.14159265359;
+function pi() {
+  screen.value = 3.14159265359;
 }
 
-function e()
-{
-    screen.value=2.71828182846;
+function e() {
+  screen.value = 2.71828182846;
 }
 
-function fact()
-{
-    var i, num, f;
-    f=1
-    num=screen.value;
-    for(i=1; i<=num; i++)
-    {
-        f=f*i;
-    }
+function fact() {
+  var i, num, f;
+  f = 1;
+  num = screen.value;
+  for (i = 1; i <= num; i++) {
+    f = f * i;
+  }
 
-    i=i-1;
+  i = i - 1;
 
-    screen.value=f;
+  screen.value = f;
 }
 function factnum(num) {
-    if (num === 0 || num === 1) {
-      return 1;
-    } else {
-      return num * factnum(num - 1);
-    }
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factnum(num - 1);
   }
-  
-  function permutation() {
-    var n = parseInt(prompt("Please enter the value of n"));
-    var r = parseInt(prompt("Please enter the value of r"));
-  
-    if (isNaN(n) || isNaN(r)) {
-      alert("Please enter valid numbers.");
-      return;
-    }
-  
-    var nfact = factnum(n);
-    var nrfact = factnum(n - r);
-    var result = nfact / nrfact;
-    screen.value = result;
-  }
-  
-  function combination() {
-    var n = parseInt(prompt("Please enter the value of n"));
-    var r = parseInt(prompt("Please enter the value of r"));
-  
-    if (isNaN(n) || isNaN(r)) {
-      alert("Please enter valid numbers.");
-      return;
-    }
-  
-    var nfact = factnum(n);
-    var rfact = factnum(r);
-    var nrfact = factnum(n - r);
-    var result = nfact / (rfact * nrfact);
-    screen.value = result/2;
-  }
-  
-function degrees_to_radians ()
- { 
-  screen.value = screen.value * (Math.PI / 180); 
 }
-function toDegrees () 
-{ 
-  screen.value = screen.value  * (180 / Math.PI);
+
+function permutation() {
+  var n = parseInt(prompt("Please enter the value of n"));
+  var r = parseInt(prompt("Please enter the value of r"));
+
+  if (isNaN(n) || isNaN(r)) {
+    alert("Please enter valid numbers.");
+    return;
+  }
+
+  var nfact = factnum(n);
+  var nrfact = factnum(n - r);
+  var result = nfact / nrfact;
+  screen.value = result;
+}
+
+function combination() {
+  var n = parseInt(prompt("Please enter the value of n"));
+  var r = parseInt(prompt("Please enter the value of r"));
+
+  if (isNaN(n) || isNaN(r)) {
+    alert("Please enter valid numbers.");
+    return;
+  }
+
+  var nfact = factnum(n);
+  var rfact = factnum(r);
+  var nrfact = factnum(n - r);
+  var result = nfact / (rfact * nrfact);
+  screen.value = result / 2;
+}
+
+function degrees_to_radians() {
+  screen.value = screen.value * (Math.PI / 180);
+}
+function toDegrees() {
+  screen.value = screen.value * (180 / Math.PI);
 }
 function findNthRoot() {
   var nthRootValue = parseFloat(screen.value);
-  var n = parseFloat(prompt("Enter the value of n (the root you want to find):"));
+  var n = parseFloat(
+    prompt("Enter the value of n (the root you want to find):")
+  );
 
   if (isNaN(nthRootValue) || isNaN(n)) {
     alert("Invalid input. Please enter valid numbers.");
@@ -251,22 +226,19 @@ function nthPower() {
   var n = parseFloat(prompt("Enter the value of n (the exponent):"));
 
   if (isNaN(base) || isNaN(n)) {
-      alert("Invalid input. Please enter valid numbers.");
-      return;
+    alert("Invalid input. Please enter valid numbers.");
+    return;
   }
 
   var result = Math.pow(base, n);
   screen.value = result;
 }
-function backspc()
-{
-    screen.value=screen.value.substr(0,screen.value.length-1);
+function backspc() {
+  screen.value = screen.value.substr(0, screen.value.length - 1);
 }
-function allclear()
-{
-  screen.value='';
+function allclear() {
+  screen.value = "";
 }
-function equality()
-{
-  screen.value=eval(screen.value);
+function equality() {
+  screen.value = eval(screen.value);
 }
